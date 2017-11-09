98004473422bbd249098f56307f013f8f53bb2da
huxizhijian <huxizhijian@yeah.net>
2017-11-09 17:48:11 +0800

    add debug activity

---
+++ app/src/main/java/org/huxizhijian/hhcomicviewer/ui/debug/DebugActivity.java
@@ -0,0 +1,19 @@
+package org.huxizhijian.hhcomicviewer.ui.debug;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.huxizhijian.hhcomicviewer.R;
+
+/**
+ * @author huxizhijian
+ * @date 2017/11/9
+ */
+public class DebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+    }
+}
