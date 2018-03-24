e880c31c46311abe7310115b3dac52ab5616825f
huxizhijian <413611323@qq.com>
2018-03-25 01:14:58 +0800

    update licenses

--- hh-core/src/main/java/org/huxizhijian/sdk/imagedownload/core/ImageDownloadRequest.java
+++ hh-core/src/main/java/org/huxizhijian/sdk/imagedownload/core/ImageDownloadRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 huxizhijian
+ * Copyright 2018 huxizhijian
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
